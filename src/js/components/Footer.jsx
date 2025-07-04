aa3fcafead3f5b48953cb4b984d0e2ce766fb72f
SleepyDut <diegoundreiner@gmail.com>
2025-07-04 17:24:13 -0400

    Create Footer.jsx

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+const Footer = () => {
+	return (
+		<footer className="mt-auto d-flex py-4 justify-content-center align-items-end bg-secondary text-light">
+			Copyright &copy; Your website 2025
+		</footer>
+	);
+};
+
+export default Footer;
